daf2b52745e049a63dce8320f6014af4bd4d9005
kobino <felipe.arg@gmail.com>
2024-10-16 12:31:29 +0200

    refactor(media-list): MediaList as a mediator between movie-card and movie-detail

--- src/components/movie-list.ts
+++ src/components/movie-list.ts
@@ -5,30 +5,37 @@ import litLogo from "../assets/lit.svg";
 import { fetchMovieList } from "../models/movies.ts";
 import { fetchMovieDetails } from "../models/movie.ts";
 
+import "./movie-card.ts";
+import "./movie-detail.ts";
+
 @customElement("movie-list")
 export class MovieList extends LitElement {
   @property({ type: Array })
   movieList: Movie[] = [];
 
-  @property({ type: Boolean })
+  @property({ type: Object })
+  movieDetailData: MovieDetailType = [];
+
+  @state()
   _openDetail: Boolean = false;
 
-  @property({
-    type: Object,
-  })
-  movieDetail: MovieDetail = [];
-
   willUpdate(changedProperties: PropertyValues<this>) {
-    if (changedProperties.has("movieDetail")) {
-      if ("id" in this.movieDetail) {
+    if (changedProperties.has("movieDetailData")) {
+      if ("id" in this.movieDetailData) {
         this._openDetail = true;
       } else {
         this._openDetail = false;
       }
     }
   }
-  _handleCloseDetail = () => {
-    this.movieDetail = [];
+  #handleCloseDetail = () => {
+    this.movieDetailData = [];
+  };
+
+  #handleChangeMovie = async (event: CustomEvent) => {
+    const movieId = event.detail.id;
+    const response: MovieDetailType = await fetchMovieDetails(movieId);
+    if ("id" in response) this.movieDetailData = response;
   };
 
   protected async firstUpdated(
@@ -37,51 +44,6 @@ export class MovieList extends LitElement {
     const response: MoviesResponse = await fetchMovieList();
     if ("results" in response) this.movieList = response.results;
   }
-
-  getImagePath = (path: string) => {
-    const basePath = "https://image.tmdb.org/t/p/w1280";
-    return basePath + path;
-  };
-
-  async #getMovieDetails(movieId: number) {
-    const response: MovieDetail = await fetchMovieDetails(movieId);
-    if ("id" in response) this.movieDetail = response;
-  }
-
-  getMovieCard = (movie: Movie) => {
-    const rating = parseFloat(movie.vote_average.toFixed(1));
-    return html`
-      <div class="movie-card" @click="${() => this.#getMovieDetails(movie.id)}">
-        <img src="${this.getImagePath(movie.poster_path)}" />
-        <div class="movie-title">
-          <span class="title">${movie.title}</span>
-          <span class="rating">${rating}</span>
-        </div>
-      </div>
-    `;
-  };
-
-  getMovieDetailHtml = () => {
-    const movie = this.movieDetail as MovieFullDetail;
-    return html`
-      <div class="movie-detail-container">
-        <div
-          class="movie-content-backdrop"
-          style="--backdrop-image:url('${this.getImagePath(
-            movie.backdrop_path
-          )}');"
-        ></div>
-        <div class="movie-content">
-          <div class="movie-content-poster">
-            <img src="${this.getImagePath(movie.poster_path)}" />
-          </div>
-          <div class="movie-content-credits">
-            <h1>${movie.title}</h1>
-          </div>
-        </div>
-      </div>
-    `;
-  };
 
   render() {
     return html`
@@ -96,11 +58,22 @@ export class MovieList extends LitElement {
             ? "movie-detail-wrapper open"
             : "movie-detail-wrapper"}"
         >
-          ${this.getMovieDetailHtml()}
-          <button @click="${this._handleCloseDetail}">Close</button>
+          <movie-detail
+            .movie="${this.movieDetailData}"
+            @close-detail=${this.#handleCloseDetail}
+          >
+          </movie-detail>
         </div>
+
         <ul class="movie-list">
-          ${this.movieList.map((movie) => this.getMovieCard(movie))}
+          ${this.movieList.map(
+            (movie) => html`
+              <movie-card
+                .movie=${movie}
+                @change-movie=${this.#handleChangeMovie}
+              ></movie-card>
+            `
+          )}
         </ul>
       </div>
     `;
@@ -132,42 +105,6 @@ export class MovieList extends LitElement {
       &.open {
         display: block;
       }
-      .movie-detail-container {
-        position: relative;
-        > .movie-content-backdrop {
-          position: absolute;
-          min-width: 100%;
-          min-height: 100%;
-          background-size: cover;
-          background-repeat: no-repeat;
-          background-image: var(--backdrop-image);
-          opacity: 0.2;
-          filter: grayscale(80%);
-        }
-        > .movie-content {
-          position: relative;
-          display: flex;
-          gap: 2rem;
-          padding: 3rem 2rem;
-          > .movie-content-poster {
-            min-width: 330px;
-            flex: 0;
-            > img {
-              background-color: #fff;
-              max-width: 100%;
-              border-radius: 14px;
-              overflow: hidden;
-            }
-          }
-          > .movie-content-credits {
-            flex: 1;
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-            align-items: flex-start;
-          }
-        }
-      }
     }
     .side-bar {
       padding-right: 1rem;
@@ -181,40 +118,6 @@ export class MovieList extends LitElement {
       gap: 1.4rem;
       max-height: 100vh;
       overflow-y: auto;
-    }
-    .movie-card {
-      border: 1px solid #dbdbdb;
-      border-radius: 12px;
-      max-width: 200px;
-      overflow: hidden;
-      position: relative;
-      z-index: 2;
-      &:hover {
-        cursor: pointer;
-        opacity: 0.8;
-        > img {
-          transform: scale(106%);
-        }
-      }
-      > img {
-        transition: transform 300ms ease-in-out;
-        width: 100%;
-        height: auto;
-      }
-      > .movie-title {
-        line-height: 1.1;
-        padding: 0.8rem 1rem 1.6rem 1rem;
-        display: flex;
-        justify-content: space-between;
-        > .title {
-          font-size: 0.8rem;
-          font-weight: 600;
-        }
-        > .rating {
-          font-weight: 900;
-          font-size: 0.8rem;
-        }
-      }
     }
   `;
   static globalStyles = css`
@@ -235,56 +138,6 @@ export class MovieList extends LitElement {
     }
     .logo.lit:hover {
       filter: drop-shadow(0 0 2em #325cffaa);
-    }
-
-    .card {
-      padding: 2em;
-    }
-
-    .read-the-docs {
-      color: #888;
-    }
-
-    ::slotted(h1) {
-      font-size: 3.2em;
-      line-height: 1.1;
-    }
-
-    a {
-      font-weight: 500;
-      color: #646cff;
-      text-decoration: inherit;
-    }
-    a:hover {
-      color: #535bf2;
-    }
-
-    button {
-      border-radius: 8px;
-      border: 1px solid transparent;
-      padding: 0.6em 1.2em;
-      font-size: 1em;
-      font-weight: 500;
-      font-family: inherit;
-      background-color: #1a1a1a;
-      cursor: pointer;
-      transition: border-color 0.25s;
-    }
-    button:hover {
-      border-color: #646cff;
-    }
-    button:focus,
-    button:focus-visible {
-      outline: 4px auto -webkit-focus-ring-color;
-    }
-
-    @media (prefers-color-scheme: light) {
-      a:hover {
-        color: #747bff;
-      }
-      button {
-        background-color: #f9f9f9;
-      }
     }
   `;
   static styles = [MovieList.globalStyles, MovieList.movieStyles];
