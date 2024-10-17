7210cab0046ae4e22eeb2fcd1908fb70c8e173ee
kobino <felipe.arg@gmail.com>
2024-10-17 02:03:44 +0200

    models(movies): New method for searching movies

--- src/models/movies.ts
+++ src/models/movies.ts
@@ -4,8 +4,31 @@ export const fetchMovieList = async (): Promise<
   MoviesResponse | EmptyResponse
 > => {
   const url = "/discover/movie";
+  const params: FetchParams = {
+    sort_by: "popularity.desc",
+  };
   try {
-    const res = await useAxios(url);
+    const res = await useAxios(url, {
+      params,
+    });
+    return res.data;
+  } catch (err: any) {
+    console.error(err.message);
+    return [];
+  }
+};
+
+export const searchMovieList = async (
+  params: SearchParams = { query: "" }
+): Promise<MoviesResponse | EmptyResponse> => {
+  const url = "search/movie";
+  try {
+    const res = await useAxios(url, {
+      params,
+    });
+    const rawResults = res.data.results;
+    rawResults.sort((a: Movie, b: Movie) => b.popularity - a.popularity);
+    res.data.results = rawResults;
     return res.data;
   } catch (err: any) {
     console.error(err.message);
